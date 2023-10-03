44987f746f574f390483662a60690153eb3bc1a8
Ibrahim Mohamed <89018417+ibrahim1118@users.noreply.github.com>
2023-10-03 13:07:18 +0300

    Time: 86 ms (13.68%), Space: 37.9 MB (94.49%) - LeetHub

---
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int NumIdenticalPairs(int[] nums) {
+      int ans =0; 
+     int n = nums.Length; 
+      for (int i=0;i<n;i++)
+      {
+          for (int j =i+1;j<n;j++)
+          {
+              if (nums[i]==nums[j])
+                  ans++; 
+          }
+      }
+        return ans;  
+    }
+}
