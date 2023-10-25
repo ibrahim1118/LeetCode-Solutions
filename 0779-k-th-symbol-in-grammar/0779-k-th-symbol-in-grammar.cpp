32f204d1e19ca3d4db65b8f130eb7e902c59adf2
Ibrahim Mohamed <89018417+ibrahim1118@users.noreply.github.com>
2023-10-25 19:35:10 +0300

    Time: 2 ms (51.37%), Space: 6.3 MB (28.86%) - LeetHub

---
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    
+    int kthGrammar(int n, int k) {
+       int l=1, r=pow(2,n-1);
+        int ans=0;
+        while (l<r) 
+        {
+            int mid=(l+r)/2;
+            if (k<=mid) r=mid;
+            else 
+            {
+                ans=1-ans;
+                l=mid+1;
+            }
+        }
+        return ans;
+    }
+ 
+};
