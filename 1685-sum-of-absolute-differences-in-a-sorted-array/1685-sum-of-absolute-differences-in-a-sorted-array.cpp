896b3d91d82acfa3f450c396454abaaf497a616c
Ibrahim Mohamed <89018417+ibrahim1118@users.noreply.github.com>
2023-11-25 11:28:38 +0200

    Time: 108 ms (8.92%), Space: 87.7 MB (33.24%) - LeetHub

--- 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.cpp
+++ 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.cpp
@@ -10,13 +10,13 @@ public:
         {
             if (i==0)
             {
-                int c =nums[i]; 
-                ans.push_back((nums[nums.size()-1]-c)-(nums.size()-i-1)*c); 
+             
+                ans.push_back((nums[nums.size()-1]-nums[i])-(nums.size()-i-1)*nums[i]); 
             }
             else if (i==nums.size()-1)
             {
-                int c = nums[i]-nums[i-1]; 
-                ans.push_back(c*i-(nums[i-1])); 
+                  
+                ans.push_back((nums[i]-nums[i-1])*i-(nums[i-1])); 
             }
             else 
             {
