763f80de08a02645aa7782ffb6d1ad90a4c0f2f9
Ibrahim Mohamed <89018417+ibrahim1118@users.noreply.github.com>
2023-11-27 14:55:13 +0200

    Time: 337 ms (61.43%), Space: 14.8 MB (92.87%) - LeetHub

---
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minFallingPathSum(vector<vector<int>>& grid) {
+       for(int i=1;i<grid.size();i++)
+       {
+           for (int j=0;j<grid[i].size();j++)
+           {
+               int a = INT_MAX; 
+               for (int k=0;k<grid[i].size();k++)
+               {
+                   if (k!=j)
+                       a = min(grid[i-1][k] , a); 
+               }
+               grid[i][j]+=a; 
+           }
+       }
+       int ans = INT_MAX; 
+       for (auto i : grid[grid.size()-1])
+           ans = min(ans , i); 
+        return ans; 
+    }
+};
