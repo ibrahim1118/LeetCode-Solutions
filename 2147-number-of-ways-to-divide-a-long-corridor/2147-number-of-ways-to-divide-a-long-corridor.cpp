9da478661846da95e935b48e305bd57a638c6238
Ibrahim Mohamed <89018417+ibrahim1118@users.noreply.github.com>
2023-11-28 16:46:18 +0200

    Time: 129 ms (61.14%), Space: 43.8 MB (50.86%) - LeetHub

---
+++ 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int numberOfWays(string c) {
+        int s =0; 
+        for (auto i : c)
+        {
+            if (i=='S')
+                s++; 
+        }
+        if (s%2||s==0)
+            return 0; 
+        if (s==2)
+            return 1; 
+        vector<int>v; 
+        int a =0 ; 
+        s=0; 
+        for (auto i : c)
+        {
+            if (i=='S')
+            {
+                if (s==2)
+                {
+                    s=1; 
+                    v.push_back(a); 
+                    a=0; 
+                }
+                else 
+                    s++; 
+            }
+            else 
+            {
+                if (s==2)
+                    a++; 
+            }
+        }
+        long long ans =1 ; 
+        int mod = 1e9+7; 
+        for (auto i : v)
+        {
+            ans = ((ans%mod)*((i+1)%mod))%mod; 
+            ans%=mod ; 
+        }
+    return (int)ans; 
+    }
+};
