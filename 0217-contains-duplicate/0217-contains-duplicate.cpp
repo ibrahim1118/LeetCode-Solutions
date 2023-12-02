76093f03b9916b1b2b2aeeb3f421c3ea9787b0cd
Ibrahim Mohamed <89018417+ibrahim1118@users.noreply.github.com>
2023-12-02 16:01:55 +0200

    Time: 102 ms (50.77%), Space: 72.1 MB (15.49%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        set<int>st; 
+        unordered_set<int>st; 
         for (auto i : nums)
             st.insert(i); 
         return st.size()<nums.size(); 
