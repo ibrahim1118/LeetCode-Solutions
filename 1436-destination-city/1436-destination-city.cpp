81b6fc9df50e923b2a706a30955dfd3e7719195e
Ibrahim Mohamed <89018417+ibrahim1118@users.noreply.github.com>
2023-12-15 15:31:05 +0200

    Time: 17 ms (7.42%), Space: 12.1 MB (10.53%) - LeetHub

---
+++ 1436-destination-city/1436-destination-city.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string destCity(vector<vector<string>>& paths) {
+      map<string,int>mp; 
+      for (auto i : paths)
+          mp[i[0]]++; 
+      for (auto i : paths)
+      {
+          if (mp[i[1]]==0)
+              return i[1]; 
+      }
+      return ""; 
+    }
+};
