b6f27b6f5407e1d68a943b65c99a2b149d99e228
Ibrahim Mohamed <89018417+ibrahim1118@users.noreply.github.com>
2024-01-04 15:29:30 +0200

    Time: 122 ms (54.33%), Space: 85.2 MB (46.04%) - LeetHub

--- 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.cpp
+++ 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums) {
-       vector<int>freq(1000006,0); 
+        unordered_map<int,int>freq; 
         for (auto i : nums)
             freq[i]++; 
         int ans =0; 
-        for (int &i : freq)
+        for (auto &i : freq)
         {
-           if (i==1)
+           if (i.second==1)
               return -1; 
-           else if (i%3==0)
-                ans+=i/3; 
-           else if (i%3==2)
+           else if (i.second%3==0)
+                ans+=i.second/3; 
+           else if (i.second%3==2)
            {
-               ans+=i/3; 
+               ans+=i.second/3; 
                ans++; 
            }
-           else if (i%3==1)
+           else if (i.second%3==1)
            {
                ans+=2; 
-               ans+=(i-4)/3; 
+               ans+=(i.second-4)/3; 
            }
         
         }
