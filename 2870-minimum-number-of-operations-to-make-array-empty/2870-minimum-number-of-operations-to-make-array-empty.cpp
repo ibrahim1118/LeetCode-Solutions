563bb2703a0c68705f6ef29c1a2efc6795804466
Ibrahim Mohamed <89018417+ibrahim1118@users.noreply.github.com>
2024-01-04 14:10:11 +0200

    Time: 1929 ms (5.16%), Space: 302.9 MB (5.16%) - LeetHub

---
+++ 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+       vector<int>freq(1000006,0); 
+        for (auto i : nums)
+            freq[i]++; 
+        int ans =0; 
+        for (auto &i : freq)
+        {
+           if (i==1)
+              return -1; 
+           else if (i%3==0)
+                ans+=i/3; 
+           else if (i%3==2)
+           {
+               ans+=i/3; 
+               ans++; 
+           }
+           else if (i%3==1)
+           {
+               ans+=2; 
+               ans+=(i-4)/3; 
+           }
+        
+        }
+        return ans; 
+    }
+};
