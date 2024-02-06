efeee1eb6cfcfc165862b0a5716b0b8c809f321a
Ibrahim Mohamed <89018417+ibrahim1118@users.noreply.github.com>
2024-02-06 18:36:48 +0200

    Time: 35 ms (38.55%), Space: 24.5 MB (17.01%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        map<string, vector<string>>mp; 
+        for (auto i : strs)
+        {
+            string s = i; 
+            sort(s.begin() , s.end());
+            mp[s].push_back(i); 
+        }
+        vector<vector<string>>ans ;
+        for (auto i : mp)
+            ans.push_back(i.second); 
+        return ans; 
+    }
+};
