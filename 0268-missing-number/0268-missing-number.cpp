0c4f6fee7894af56a0378837a4ae9ef6b6789968
Ibrahim Mohamed <89018417+ibrahim1118@users.noreply.github.com>
2024-02-20 11:47:47 +0200

    Time: 7 ms (97.83%), Space: 20.2 MB (54.51%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int ans = nums.size()*(nums.size()+1)/2; 
-        for (int i : nums)
+        for (auto i : nums)
         {
             ans-=i; 
         }
