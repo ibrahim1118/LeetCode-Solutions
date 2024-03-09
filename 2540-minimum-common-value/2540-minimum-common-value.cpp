220678dba106c40ae6d3f3b8651ca6b850e20617
Ibrahim Mohamed <89018417+ibrahim1118@users.noreply.github.com>
2024-03-09 19:40:17 +0200

    Time: 116 ms (20.28%), Space: 69.1 MB (14.66%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.cpp
+++ 2540-minimum-common-value/2540-minimum-common-value.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
-        map<int,int>mp; 
+        unordered_map<int,int>mp; 
         for (auto i : nums1)
               mp[i]++; 
         for (auto i : nums2)
