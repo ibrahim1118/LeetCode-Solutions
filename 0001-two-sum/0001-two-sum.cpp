341cc2ef102d6e31bb0bd07572bfe0ce8a999cec
Ibrahim Mohamed <89018417+ibrahim1118@users.noreply.github.com>
2024-03-28 23:39:01 +0200

    Time: 0 ms (100.00%), Space: 13.6 MB (43.81%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
       map<int,int>mp;  
-         //x   i
+   
       for (int i=0;i<nums.size();i++)
       { 
          int x = target  - nums[i]; 
