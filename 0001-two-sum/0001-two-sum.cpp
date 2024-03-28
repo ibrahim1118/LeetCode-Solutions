97439354606a2a98bd4f853bbdf5ae7b7b349c48
Ibrahim Mohamed <89018417+ibrahim1118@users.noreply.github.com>
2024-03-28 23:21:23 +0200

    Time: 202 ms (8.10%), Space: 12.7 MB (74.00%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,42 +1,22 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-       vector<int>ans; 
-       vector<int> aa = nums; 
-       int a ,b; 
-       sort(nums.begin(),nums.end()); 
-       for (int i=0,j=nums.size()-1;i<j;)
-       {
-           if (nums[i]+nums[j]== target)
-           {
-               a= nums[i]; 
-               b= nums[j]; 
-               break;
-           }
-           else if (nums[i]+nums[j]>target)
-           {
-               j--; 
-           }
-           else 
-           {
-              i++;
-           }
-       }
-        for (int i=0;i<nums.size();i++)
-        {
-            if (a==aa[i])
-              { ans.push_back(i);
-                   break; 
-              } 
-        }
-        for (int i=nums.size()-1;i>=0;i--)
-        {
-            if (b==aa[i])
-              { 
-                  ans.push_back(i);
-                  break;  
+      for (int i=0;i<nums.size();i++)
+      {
+          for (int j=0;j<nums.size();j++)
+          {
+              if (i!=j)
+              {
+                  if (nums[i]+nums[j]==target)
+                  {
+                      vector<int>ans;
+                      ans.push_back(i);  
+                      ans.push_back(j);
+                      return ans; 
+                  }
               }
-        }
-      return  ans; 
+          }
+      }
+      return {};  
     }
 };
