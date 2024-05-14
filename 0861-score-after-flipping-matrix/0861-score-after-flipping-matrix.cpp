110e2080d6a418384a3e0441fbbfbd880133069b
Ibrahim Mohamed <89018417+ibrahim1118@users.noreply.github.com>
2024-05-14 21:29:53 +0300

    Time: 3 ms (41.62%), Space: 10.4 MB (9.18%) - LeetHub

--- 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.cpp
+++ 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.cpp
@@ -1,7 +1,13 @@
 class Solution {
 public:
-    int matrixScore(vector<vector<int>>& grid) {
-       int n  = grid.size(); 
+    vector<vector<int>>grid; 
+    int matrixScore(vector<vector<int>>& _grid) {
+       grid = _grid; 
+        return solve(); 
+    }
+    int solve()
+    {
+         int n  = grid.size(); 
        int m = grid[0].size(); 
         int ans =0; 
        for (int i=0;i<n;i++)
