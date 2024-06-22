aaa84a0f382b439cfde592d1f3970d34fb8413c8
Ibrahim Mohamed <89018417+ibrahim1118@users.noreply.github.com>
2024-06-22 13:25:04 +0300

    Time: 87 ms (86.24%), Space: 86.6 MB (18.43%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -5,15 +5,14 @@ public:
         for (auto i : nums)
             v.push_back(i%2); 
         for (int i=1;i<v.size();i++)
-               v[i]+=v[i-1]; 
-        for (auto i : v)
-            cout<<i<<' '; 
+               v[i]+=v[i-1];
         
-        unordered_map<int,int>mp ;
+        vector<int>mp(50005) ;
         mp[0] =  1 ; 
         int ans =0;
         for (auto i :v )
         {
+            if (i>=k)
             ans+=mp[i-k]; 
             mp[i]++; 
         }
