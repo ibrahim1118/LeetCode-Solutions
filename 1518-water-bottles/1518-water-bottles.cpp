4c0a154d82dfb6be9eb55adc391002e9a1081f32
Ibrahim Mohamed <89018417+ibrahim1118@users.noreply.github.com>
2024-07-07 14:04:50 +0300

    Time: 3 ms (13.78%), Space: 7.2 MB (30.43%) - LeetHub

---
+++ 1518-water-bottles/1518-water-bottles.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numWaterBottles(int numB, int numE) {
+        int ans =0; 
+        while(numB>0)
+        {
+            ans+=(numB/numE)*numE;
+            numB = numB/numE + numB%numE; 
+            if (numB<numE)
+            {
+                ans+=numB; 
+                break; 
+            }
+            
+        }
+        return ans; 
+    }
+};
