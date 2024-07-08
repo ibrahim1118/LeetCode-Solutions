363b13b1026bb54ac2c3b550d523f5a425974b72
Ibrahim Mohamed <89018417+ibrahim1118@users.noreply.github.com>
2024-07-08 17:24:54 +0300

    Time: 21 ms (28.58%), Space: 25.6 MB (29.89%) - LeetHub

---
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findTheWinner(int n, int k) {
+        queue<int> q;
+      for(int i=1;i<=n;++i)
+             q.push(i);
+        while(q.size()>=2){
+            for(int i=1;i<k;++i){
+                int x = q.front();
+                q.pop();
+                q.push(x);
+            }
+            q.pop();
+        }
+        return q.front(); 
+    }
+};
