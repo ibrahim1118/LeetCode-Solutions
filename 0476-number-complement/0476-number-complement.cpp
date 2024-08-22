e709e15dfccb9b7898e1c645cd0e3bd7c98ae277
Ibrahim Mohamed <89018417+ibrahim1118@users.noreply.github.com>
2024-08-22 14:15:09 +0300

    Time: 2 ms (50.07%), Space: 7.4 MB (57.13%) - LeetHub

--- 0476-number-complement/0476-number-complement.cpp
+++ 0476-number-complement/0476-number-complement.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int findComplement(int num) {
-     int ans =0; 
+     
       int f =0 ; 
       bitset<32> n = num; 
       for (int i=31;i>=0;i--)
       {
           if (n[i]==1)
-              f++; 
-          if (n[i]==0&&f)
-              ans+=pow(2 , i); 
+              f++;
+          if (f)
+           n[i] = !n[i];  
       }
-      return ans; 
+      return num = n.to_ulong(); 
           
     }
 };
