c796e67269d62937b4ef2a8e7238a14f003f414c
Ibrahim Mohamed <89018417+ibrahim1118@users.noreply.github.com>
2024-12-07 13:09:05 +0200

    Time: 39 ms (41.78%), Space: 59.8 MB (15.54%) - LeetHub

--- 1760-minimum-limit-of-balls-in-a-bag/1760-minimum-limit-of-balls-in-a-bag.cpp
+++ 1760-minimum-limit-of-balls-in-a-bag/1760-minimum-limit-of-balls-in-a-bag.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
-    int minimumSize(vector<int>& nums, int ma) {
-        int r = *max_element(nums.begin(), nums.end());
-        int l = 1;
-        int ans = r;
-        while(l < r) {
-            int mid = (l + r) / 2;
-            int d = 0;
-            for(int i = 0; i < nums.size(); i++) {
-                if(nums[i] > mid) {
-                    if(nums[i] % mid == 0) {
-                        d += nums[i] / mid - 1;
-                    } else {
-                    d += nums[i] / mid;
-                    }
-                }
+    int minimumSize(vector<int>& nums, int maxOperations) {
+        int ans= 1e9; 
+        int l =1 , r = 1e9; 
+        while(l<=r)
+        {
+            int mid = l+(r-l)/2; 
+            int x =0 ;
+            for (auto i: nums)
+            {
+                x+=i/mid; 
+                if (i%mid==0)
+                    x-=1; 
             }
-            if(d <= ma) {
-                ans = mid;
-                r = mid;
-            } else {
-                l = mid + 1;
+            if (x<=maxOperations)
+            {
+                ans =min(mid ,ans); 
+                r = mid-1; 
             }
+            else 
+                l = mid+1; 
         }
-        return ans;
+        return ans; 
     }
 };
